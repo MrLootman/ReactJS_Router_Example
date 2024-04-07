699741e645cebf02abe7024201773cae527e3890
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2024-04-07 17:45:02 +0200

    First version of routing test

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import '../styles/header.css';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <a>Home</a>
+        <a>Connexion</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
